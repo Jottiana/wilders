c442b4fa1a27c339006cf96535e79fa4bc499c27
jottiana <11.paradise.avenue@gmail.com>
2024-10-01 12:04:41 +0200

    create wilders project + add names

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.rgb(50, 133, 168).bold.bgBlack("July"));
+console.log(chalk.rgb(50, 168, 107).bold.bgBlack("Gege"));
+console.log(chalk.rgb(214, 60, 160).bold.bgBlack("Line"));
+console.log(chalk.rgb(212, 212, 40).bold.bgBlack("Mala"));
